b0e55924299341245fff5991b5666c1ef2853b42
Jesse Morgan <jesse@jesterpm.net>
2013-09-16 20:52:58 -0700

    Adding frame grab images to videos and fixing broken links

--- devfiles/scripts/videos-from-csv.py
+++ devfiles/scripts/videos-from-csv.py
@@ -8,12 +8,13 @@ from string import Template
 
 BASE_URL="http://foursquaregrow.s3-website-us-east-1.amazonaws.com/"
 
-def mkjson(chapter, number, title, length, pdf, videos):
+def mkjson(chapter, number, title, length, image, pdf, videos):
     vtemplate = Template("""{
     "id": "$id",
     "number": "$number",
     "title": "$title",
     "length": $length,
+    "image": "$image",
     "pdf": "$pdf",
     "urls": [""")
 
@@ -30,7 +31,8 @@ def mkjson(chapter, number, title, length, pdf, videos):
     filename = string.lower(directory + "/" + chapter + "-" + number + ".json")
     with open(filename, 'w') as outfile:
         outfile.write(vtemplate.substitute(dict(id=string.lower(chapter+"-"+number),
-            chapter=chapter, number=number, title=title, length=length, pdf=BASE_URL + urllib.quote(pdf))))
+            chapter=chapter, number=number, title=title, length=length,
+            image=BASE_URL + urllib.quote(image), pdf=BASE_URL + urllib.quote(pdf))))
 
         for type,src in videos.iteritems():
             outfile.write(urltemplate.substitute(dict(type=type, src=BASE_URL + urllib.quote(src))))
@@ -49,10 +51,11 @@ with open(filename, 'rb') as csvfile:
         number = row[1]
         title = row[2]
         length = row[3]
-        pdf = row[4]
-        h264 = row[5]
+        image = row[4]
+        pdf = row[5]
+        h264 = row[6]
 
         videos = { "video/mp4": h264 }
 
-        mkjson(chapter, number, title, length, pdf, videos)
+        mkjson(chapter, number, title, length, image, pdf, videos)
 
