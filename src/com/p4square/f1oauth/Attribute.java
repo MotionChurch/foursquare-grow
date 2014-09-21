a5c349b8a1ec1cdfde396c42812e83c81aa31cb3
Jesse Morgan <jesse@jesterpm.net>
2014-09-21 15:10:58 -0700

    Removing comment.

--- src/com/p4square/f1oauth/Attribute.java
+++ src/com/p4square/f1oauth/Attribute.java
@@ -12,33 +12,6 @@ import java.util.Date;
  * @author Jesse Morgan <jesse@jesterpm.net>
  */
 public class Attribute {
-    /*Attribute
-     *{
-    "attribute": {
-        "@id": "",
-        "@uri": "",
-        "person": {
-            "@id": "1636208",
-            "@uri": "https://demo.fellowshiponeapi.com/v1/People/1636208"
-        },
-        "attributeGroup": {
-            "@id": "",
-            "@uri": "",
-            "name": null,
-            "attribute": {
-                "@id": "958",
-                "@uri": "",
-                "name": null
-            }
-        },
-        "startDate": null,
-        "endDate": null,
-        "comment": null,
-        "createdDate": null,
-        "lastUpdatedDate": null
-    }
-    */
-
     private Date mStartDate;
     private Date mEndDate;
     private String mComment;
